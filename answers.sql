e6ab305c79b4a1820439c518f8c77ad4370019ed
musa11it <142474478+musa11it@users.noreply.github.com>
2025-04-24 14:40:29 +0200

    week1 submission

---
+++ answers.sql
@@ -0,0 +1,7 @@
+-- 📝 Assignment: Introduction to Databases
+
+-- 1️⃣ Create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- 2️⃣ Drop (delete) the database called demo
+DROP DATABASE demo;
